7d5cc58863b9269725772f4b28d57bd52eb2291c
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2024-01-25 01:27:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ selected_strike = st.selectbox('Select Strike Price', list(map(float, strike_dat
 
 # Table header
 columns = ['Option Type', 'Instrument Key', 'LTP', 'Bid Price', 'Bid Qty', 'Ask Price', 'Ask Qty', 'Volume', 'OI', 'Prev OI', 'OI Change',
-           'Sentiment', 'Trend', 'Delta', 'Gamma', 'Vega', 'Theta', 'IV', 'PCR', 'Sentiment Condition', 'Vol OI Analysis']
+           'Sentiment', 'Trend', 'Delta', 'Gamma', 'Vega', 'Theta', 'IV', 'PCR', 'Sentiment Condition', 'Vol OI Analysis',
+           'Call Sum | Calculated', 'Put Sum | Calculated', 'Options Flow Diff | Calculated']
 table_data = []
 
 # Populate the table with data for the selected strike
@@ -31,20 +32,11 @@ strike_info = strike_data[str(selected_strike)]
 call_data = strike_info.get('callOptionData', {})
 instrument_key_call = call_data.get('instrumentKey', '')
 market_data_call = call_data.get('marketData', {})
-analytics_call = call_data.get('analytics', {})
 oi_call = market_data_call.get('oi', 0)
 prev_oi_call = market_data_call.get('prevOi', 0)
 oi_change_call = oi_call - prev_oi_call
-sentiment_call = 'Bullish' if oi_change_call > 0 else 'Bearish' if oi_change_call < 0 else 'Neutral'
 volume_call = market_data_call.get('volume', 0)
-trend_call = 'Day Trend' if volume_call > 0 and oi_call <= volume_call else 'Long-Term Trend' if volume_call > 0 and oi_call > volume_call else 'No Trend'
-delta_call = analytics_call.get('delta', 0)
-gamma_call = analytics_call.get('gamma', 0)
-vega_call = analytics_call.get('vega', 0)
-theta_call = analytics_call.get('theta', 0)
-iv_call = analytics_call.get('iv', 0)
-pcr_call = strike_info.get('pcr', None)
-sentiment_condition_call = 'Not Defined'  # You can add conditions based on user preferences
+delta_call = call_data.get('analytics', {}).get('delta', 0)
 
 # New condition for Vol OI Analysis
 vol_oi_analysis_call = ''
@@ -55,10 +47,19 @@ elif (volume_call > threshold_high and oi_change_call < 0) or (volume_call < thr
 else:
     vol_oi_analysis_call = 'No Clear Signal'
 
-# Add data to the table
+# Calculate Call Sum | Calculated
+call_sum_calculated = 0
+for i in range(int(selected_strike), int(selected_strike) + 3):
+    if str(i) in strike_data:
+        call_sum_calculated += strike_data[str(i)].get('callOptionData', {}).get('marketData', {}).get('oi', 0) - strike_data[str(i)].get('callOptionData', {}).get('marketData', {}).get('prevOi', 0)
+
+# Add data to the table for Call Option
 row_call = ['Call', instrument_key_call, market_data_call.get('ltp', 0), market_data_call.get('bidPrice', 0), market_data_call.get('bidQty', 0),
             market_data_call.get('askPrice', 0), market_data_call.get('askQty', 0), volume_call, oi_call, prev_oi_call, oi_change_call,
-            sentiment_call, trend_call, delta_call, gamma_call, vega_call, theta_call, iv_call, pcr_call, sentiment_condition_call, vol_oi_analysis_call]
+            'Bullish' if oi_change_call > 0 else 'Bearish' if oi_change_call < 0 else 'Neutral', 'Day Trend' if volume_call > 0 and oi_call <= volume_call else 'Long-Term Trend' if volume_call > 0 and oi_call > volume_call else 'No Trend',
+            delta_call, call_data.get('analytics', {}).get('gamma', 0), call_data.get('analytics', {}).get('vega', 0), call_data.get('analytics', {}).get('theta', 0),
+            call_data.get('analytics', {}).get('iv', 0), strike_info.get('pcr', None), 'Not Defined', vol_oi_analysis_call,
+            call_sum_calculated, 0, 0]  # Initialize Put Sum and Options Flow Diff to 0
 
 table_data.append(row_call)
 
@@ -66,20 +67,11 @@ table_data.append(row_call)
 put_data = strike_info.get('putOptionData', {})
 instrument_key_put = put_data.get('instrumentKey', '')
 market_data_put = put_data.get('marketData', {})
-analytics_put = put_data.get('analytics', {})
 oi_put = market_data_put.get('oi', 0)
 prev_oi_put = market_data_put.get('prevOi', 0)
 oi_change_put = oi_put - prev_oi_put
-sentiment_put = 'Bullish' if oi_change_put > 0 else 'Bearish' if oi_change_put < 0 else 'Neutral'
 volume_put = market_data_put.get('volume', 0)
-trend_put = 'Day Trend' if volume_put > 0 and oi_put <= volume_put else 'Long-Term Trend' if volume_put > 0 and oi_put > volume_put else 'No Trend'
-delta_put = analytics_put.get('delta', 0)
-gamma_put = analytics_put.get('gamma', 0)
-vega_put = analytics_put.get('vega', 0)
-theta_put = analytics_put.get('theta', 0)
-iv_put = analytics_put.get('iv', 0)
-pcr_put = strike_info.get('pcr', None)
-sentiment_condition_put = 'Not Defined'  # You can add conditions based on user preferences
+delta_put = put_data.get('analytics', {}).get('delta', 0)
 
 # New condition for Vol OI Analysis for Put Option
 vol_oi_analysis_put = ''
@@ -90,10 +82,22 @@ elif (volume_put > threshold_high and oi_change_put < 0) or (volume_put < thresh
 else:
     vol_oi_analysis_put = 'No Clear Signal'
 
+# Calculate Put Sum | Calculated
+put_sum_calculated = 0
+for i in range(int(selected_strike) - 2, int(selected_strike) + 1):
+    if str(i) in strike_data:
+        put_sum_calculated += strike_data[str(i)].get('putOptionData', {}).get('marketData', {}).get('oi', 0) - strike_data[str(i)].get('putOptionData', {}).get('marketData', {}).get('prevOi', 0)
+
+# Calculate Options Flow Difference Analysis | Calculated
+options_flow_diff_calculated = call_sum_calculated - put_sum_calculated
+
 # Add data to the table for Put Option
 row_put = ['Put', instrument_key_put, market_data_put.get('ltp', 0), market_data_put.get('bidPrice', 0), market_data_put.get('bidQty', 0),
            market_data_put.get('askPrice', 0), market_data_put.get('askQty', 0), volume_put, oi_put, prev_oi_put, oi_change_put,
-           sentiment_put, trend_put, delta_put, gamma_put, vega_put, theta_put, iv_put, pcr_put, sentiment_condition_put, vol_oi_analysis_put]
+           'Bullish' if oi_change_put > 0 else 'Bearish' if oi_change_put < 0 else 'Neutral', 'Day Trend' if volume_put > 0 and oi_put <= volume_put else 'Long-Term Trend' if volume_put > 0 and oi_put > volume_put else 'No Trend',
+           delta_put, put_data.get('analytics', {}).get('gamma', 0), put_data.get('analytics', {}).get('vega', 0), put_data.get('analytics', {}).get('theta', 0),
+           put_data.get('analytics', {}).get('iv', 0), strike_info.get('pcr', None), 'Not Defined', vol_oi_analysis_put,
+           0, put_sum_calculated, options_flow_diff_calculated]
 
 table_data.append(row_put)
 
