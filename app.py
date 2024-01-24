86dbfa24071e8f0bdcfdb8f756609b662ed9fdfb
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2024-01-25 01:40:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,7 @@ selected_strike = st.selectbox('Select Strike Price', list(map(float, strike_dat
 
 # Table header
 columns = ['Option Type', 'Instrument Key', 'LTP', 'Bid Price', 'Bid Qty', 'Ask Price', 'Ask Qty', 'Volume', 'OI', 'Prev OI', 'OI Change',
-           'Sentiment', 'Trend', 'Delta', 'Gamma', 'Vega', 'Theta', 'IV', 'PCR', 'Sentiment Condition', 'Vol OI Analysis',
-           'Call Sum | Calculated', 'Put Sum | Calculated', 'Options Flow Diff | Calculated']
+           'Sentiment', 'Trend', 'Delta', 'Gamma', 'Vega', 'Theta', 'IV', 'PCR', 'Sentiment Condition', 'Vol OI Analysis']
 table_data = []
 
 # Populate the table with data for the selected strike
@@ -47,19 +46,12 @@ elif (volume_call > threshold_high and oi_change_call < 0) or (volume_call < thr
 else:
     vol_oi_analysis_call = 'No Clear Signal'
 
-# Calculate Call Sum | Calculated
-call_sum_calculated = 0
-for i in range(int(selected_strike), int(selected_strike) - 3, -1):
-    if str(i) in strike_data:
-        call_sum_calculated += strike_data[str(i)].get('callOptionData', {}).get('marketData', {}).get('oi', 0) - strike_data[str(i)].get('callOptionData', {}).get('marketData', {}).get('prevOi', 0)
-
 # Add data to the table for Call Option
 row_call = ['Call', instrument_key_call, market_data_call.get('ltp', 0), market_data_call.get('bidPrice', 0), market_data_call.get('bidQty', 0),
             market_data_call.get('askPrice', 0), market_data_call.get('askQty', 0), volume_call, oi_call, prev_oi_call, oi_change_call,
             'Bullish' if oi_change_call > 0 else 'Bearish' if oi_change_call < 0 else 'Neutral', 'Day Trend' if volume_call > 0 and oi_call <= volume_call else 'Long-Term Trend' if volume_call > 0 and oi_call > volume_call else 'No Trend',
             delta_call, call_data.get('analytics', {}).get('gamma', 0), call_data.get('analytics', {}).get('vega', 0), call_data.get('analytics', {}).get('theta', 0),
-            call_data.get('analytics', {}).get('iv', 0), strike_info.get('pcr', None), 'Not Defined', vol_oi_analysis_call,
-            call_sum_calculated, 0, 0]  # Initialize Put Sum and Options Flow Diff to 0
+            call_data.get('analytics', {}).get('iv', 0), strike_info.get('pcr', None), 'Not Defined', vol_oi_analysis_call]
 
 table_data.append(row_call)
 
@@ -82,22 +74,12 @@ elif (volume_put > threshold_high and oi_change_put < 0) or (volume_put < thresh
 else:
     vol_oi_analysis_put = 'No Clear Signal'
 
-# Calculate Put Sum | Calculated
-put_sum_calculated = 0
-for i in range(int(selected_strike), int(selected_strike) + 3):
-    if str(i) in strike_data:
-        put_sum_calculated += strike_data[str(i)].get('putOptionData', {}).get('marketData', {}).get('oi', 0) - strike_data[str(i)].get('putOptionData', {}).get('marketData', {}).get('prevOi', 0)
-
-# Calculate Options Flow Difference Analysis | Calculated
-options_flow_diff_calculated = call_sum_calculated - put_sum_calculated
-
 # Add data to the table for Put Option
 row_put = ['Put', instrument_key_put, market_data_put.get('ltp', 0), market_data_put.get('bidPrice', 0), market_data_put.get('bidQty', 0),
            market_data_put.get('askPrice', 0), market_data_put.get('askQty', 0), volume_put, oi_put, prev_oi_put, oi_change_put,
            'Bullish' if oi_change_put > 0 else 'Bearish' if oi_change_put < 0 else 'Neutral', 'Day Trend' if volume_put > 0 and oi_put <= volume_put else 'Long-Term Trend' if volume_put > 0 and oi_put > volume_put else 'No Trend',
            delta_put, put_data.get('analytics', {}).get('gamma', 0), put_data.get('analytics', {}).get('vega', 0), put_data.get('analytics', {}).get('theta', 0),
-           put_data.get('analytics', {}).get('iv', 0), strike_info.get('pcr', None), 'Not Defined', vol_oi_analysis_put,
-           0, put_sum_calculated, options_flow_diff_calculated]
+           put_data.get('analytics', {}).get('iv', 0), strike_info.get('pcr', None), 'Not Defined', vol_oi_analysis_put]
 
 table_data.append(row_put)
 
