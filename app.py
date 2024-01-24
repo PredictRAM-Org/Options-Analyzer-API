e27aa7299b3406d1189e7e4ce434a19e1ea8b866
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2024-01-25 00:51:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,99 +9,66 @@ with open('upstox_data.json', 'r') as file:
 strike_data = data['data']['strategyChainData']['strikeMap']
 
 # Display the table using Streamlit
-st.title('Option Chain Data')
-st.write("Showing 'callOptionData' and 'putOptionData' for all strikes with sentiment conditions based on Delta and Change in Open Interest.")
+st.title('Option Chain Data Analysis')
+st.write("Select a strike price to view detailed information for both call and put options.")
+
+# Slider to select the strike price
+selected_strike = st.slider('Select Strike Price', min_value=min(strike_data.keys()), max_value=max(strike_data.keys()))
 
 # Table header
-columns = ['Strike', 'Option Type', 'Instrument Key', 'LTP', 'Bid Price', 'Bid Qty', 'Ask Price', 'Ask Qty', 'Volume', 'OI', 'Prev OI', 'OI Change', 'Sentiment', 'Trend', 'Delta', 'Gamma', 'Vega', 'Theta', 'IV', 'PCR', 'Sentiment Condition']
+columns = ['Option Type', 'Instrument Key', 'LTP', 'Bid Price', 'Bid Qty', 'Ask Price', 'Ask Qty', 'Volume', 'OI', 'Prev OI', 'OI Change', 'Sentiment', 'Trend', 'Delta', 'Gamma', 'Vega', 'Theta', 'IV', 'PCR', 'Sentiment Condition']
 table_data = []
 
-# Populate the table with data
-for strike, strike_info in strike_data.items():
-    call_data = strike_info.get('callOptionData', {})
-    put_data = strike_info.get('putOptionData', {})
-    
-    # Add Call Option Data
-    instrument_key_call = call_data.get('instrumentKey', '')
-    market_data_call = call_data.get('marketData', {})
-    analytics_call = call_data.get('analytics', {})
-    pcr_call = strike_info.get('pcr', None)
-    oi_call = market_data_call.get('oi', 0)
-    prev_oi_call = market_data_call.get('prevOi', 0)
-    oi_change_call = oi_call - prev_oi_call
-    sentiment_call = 'Bullish' if oi_change_call > 0 else 'Bearish' if oi_change_call < 0 else 'Neutral'
-    
-    # Determine trend for Call Option
-    volume_call = market_data_call.get('volume', 0)
-    trend_call = 'Day Trend' if volume_call > 0 and oi_call <= volume_call else 'Long-Term Trend' if volume_call > 0 and oi_call > volume_call else 'No Trend'
-    
-    # Extract relevant analytics data
-    delta_call = analytics_call.get('delta', 0)
-    gamma_call = analytics_call.get('gamma', 0)
-    vega_call = analytics_call.get('vega', 0)
-    theta_call = analytics_call.get('theta', 0)
-    iv_call = analytics_call.get('iv', 0)
+# Populate the table with data for the selected strike
+strike_info = strike_data[str(selected_strike)]
 
-    # Determine sentiment based on conditions
-    if delta_call >= 0.7 and oi_change_call > 0:
-        sentiment_condition_call = 'Strong Bullish'
-    elif 0.3 <= delta_call < 0.7 and oi_change_call > 0:
-        sentiment_condition_call = 'Mild Bullish'
-    elif -0.3 <= delta_call <= 0.3 and oi_change_call == 0:
-        sentiment_condition_call = 'Neutral'
-    elif -0.7 <= delta_call < -0.3 and oi_change_call < 0:
-        sentiment_condition_call = 'Mild Bearish'
-    elif delta_call < -0.7 and oi_change_call < 0:
-        sentiment_condition_call = 'Strong Bearish'
-    else:
-        sentiment_condition_call = 'Not Defined'
+# Call Option Data
+call_data = strike_info.get('callOptionData', {})
+instrument_key_call = call_data.get('instrumentKey', '')
+market_data_call = call_data.get('marketData', {})
+analytics_call = call_data.get('analytics', {})
+oi_call = market_data_call.get('oi', 0)
+prev_oi_call = market_data_call.get('prevOi', 0)
+oi_change_call = oi_call - prev_oi_call
+sentiment_call = 'Bullish' if oi_change_call > 0 else 'Bearish' if oi_change_call < 0 else 'Neutral'
+volume_call = market_data_call.get('volume', 0)
+trend_call = 'Day Trend' if volume_call > 0 and oi_call <= volume_call else 'Long-Term Trend' if volume_call > 0 and oi_call > volume_call else 'No Trend'
+delta_call = analytics_call.get('delta', 0)
+gamma_call = analytics_call.get('gamma', 0)
+vega_call = analytics_call.get('vega', 0)
+theta_call = analytics_call.get('theta', 0)
+iv_call = analytics_call.get('iv', 0)
+pcr_call = strike_info.get('pcr', None)
+sentiment_condition_call = 'Not Defined'  # You can add conditions based on user preferences
 
-    row_call = [strike, 'Call', instrument_key_call, market_data_call.get('ltp', 0), market_data_call.get('bidPrice', 0), market_data_call.get('bidQty', 0),
-           market_data_call.get('askPrice', 0), market_data_call.get('askQty', 0), volume_call, oi_call, prev_oi_call, oi_change_call,
-           sentiment_call, trend_call, delta_call, gamma_call, vega_call, theta_call, iv_call, pcr_call, sentiment_condition_call]
-    
-    table_data.append(row_call)
-    
-    # Add Put Option Data
-    instrument_key_put = put_data.get('instrumentKey', '')
-    market_data_put = put_data.get('marketData', {})
-    analytics_put = put_data.get('analytics', {})
-    pcr_put = strike_info.get('pcr', None)
-    oi_put = market_data_put.get('oi', 0)
-    prev_oi_put = market_data_put.get('prevOi', 0)
-    oi_change_put = oi_put - prev_oi_put
-    sentiment_put = 'Bullish' if oi_change_put > 0 else 'Bearish' if oi_change_put < 0 else 'Neutral'
-    
-    # Determine trend for Put Option
-    volume_put = market_data_put.get('volume', 0)
-    trend_put = 'Day Trend' if volume_put > 0 and oi_put <= volume_put else 'Long-Term Trend' if volume_put > 0 and oi_put > volume_put else 'No Trend'
-    
-    # Extract relevant analytics data
-    delta_put = analytics_put.get('delta', 0)
-    gamma_put = analytics_put.get('gamma', 0)
-    vega_put = analytics_put.get('vega', 0)
-    theta_put = analytics_put.get('theta', 0)
-    iv_put = analytics_put.get('iv', 0)
+# Put Option Data
+put_data = strike_info.get('putOptionData', {})
+instrument_key_put = put_data.get('instrumentKey', '')
+market_data_put = put_data.get('marketData', {})
+analytics_put = put_data.get('analytics', {})
+oi_put = market_data_put.get('oi', 0)
+prev_oi_put = market_data_put.get('prevOi', 0)
+oi_change_put = oi_put - prev_oi_put
+sentiment_put = 'Bullish' if oi_change_put > 0 else 'Bearish' if oi_change_put < 0 else 'Neutral'
+volume_put = market_data_put.get('volume', 0)
+trend_put = 'Day Trend' if volume_put > 0 and oi_put <= volume_put else 'Long-Term Trend' if volume_put > 0 and oi_put > volume_put else 'No Trend'
+delta_put = analytics_put.get('delta', 0)
+gamma_put = analytics_put.get('gamma', 0)
+vega_put = analytics_put.get('vega', 0)
+theta_put = analytics_put.get('theta', 0)
+iv_put = analytics_put.get('iv', 0)
+pcr_put = strike_info.get('pcr', None)
+sentiment_condition_put = 'Not Defined'  # You can add conditions based on user preferences
 
-    # Determine sentiment based on conditions
-    if delta_put >= 0.7 and oi_change_put > 0:
-        sentiment_condition_put = 'Strong Bullish'
-    elif 0.3 <= delta_put < 0.7 and oi_change_put > 0:
-        sentiment_condition_put = 'Mild Bullish'
-    elif -0.3 <= delta_put <= 0.3 and oi_change_put == 0:
-        sentiment_condition_put = 'Neutral'
-    elif -0.7 <= delta_put < -0.3 and oi_change_put < 0:
-        sentiment_condition_put = 'Mild Bearish'
-    elif delta_put < -0.7 and oi_change_put < 0:
-        sentiment_condition_put = 'Strong Bearish'
-    else:
-        sentiment_condition_put = 'Not Defined'
-
-    row_put = [strike, 'Put', instrument_key_put, market_data_put.get('ltp', 0), market_data_put.get('bidPrice', 0), market_data_put.get('bidQty', 0),
+# Add data to the table
+row_call = ['Call', instrument_key_call, market_data_call.get('ltp', 0), market_data_call.get('bidPrice', 0), market_data_call.get('bidQty', 0),
+            market_data_call.get('askPrice', 0), market_data_call.get('askQty', 0), volume_call, oi_call, prev_oi_call, oi_change_call,
+            sentiment_call, trend_call, delta_call, gamma_call, vega_call, theta_call, iv_call, pcr_call, sentiment_condition_call]
+row_put = ['Put', instrument_key_put, market_data_put.get('ltp', 0), market_data_put.get('bidPrice', 0), market_data_put.get('bidQty', 0),
            market_data_put.get('askPrice', 0), market_data_put.get('askQty', 0), volume_put, oi_put, prev_oi_put, oi_change_put,
            sentiment_put, trend_put, delta_put, gamma_put, vega_put, theta_put, iv_put, pcr_put, sentiment_condition_put]
-    
-    table_data.append(row_put)
+
+table_data.extend([row_call, row_put])
 
 # Display the table with headings
 st.table([columns] + table_data)
